9ee6b32b0e5ee8f867b3339bf30d624f21369692
Dylan Cadot <cadotdylan7@gmail.com>
2026-05-17 01:59:31 +0200

    feat: modifications

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const mysql = require('mysql2/promise');
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 // Crée un "pool" de connexions MySQL
 // Un pool = plusieurs connexions disponibles en même temps
 const pool = mysql.createPool({
